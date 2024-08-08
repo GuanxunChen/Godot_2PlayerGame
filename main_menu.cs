b5f4aae167a1f5d451d12049053be6ea8b0d4af6
GuanxunChen <jadkkddhdh@gmail.com>
2024-08-07 21:03:04 -0400

    Update main_menu.cs
    linked start button

--- main_menu.cs
+++ main_menu.cs
@@ -3,24 +3,39 @@ using System;
 
 public partial class main_menu : Control
 {
-	// Called when the node enters the scene tree for the first time.public override void _Ready()
-	public override void _Ready()
+    // Path to the game scene
+    private const string GameScenePath = "res://game.tscn";
+
+    public override void _Ready()
     {
-        /*Button startGameButton = GetNode<Button>("Start Game");
-        startGameButton.Connect("pressed", this, nameof(OnStartGameButtonPressed));*/
+		// Get Buttons
+        Button startButton = GetNode<Button>("Button Menu/Start Game");
+
+		// Buttons pressed
+        startButton.Pressed += OnStartButtonPressed;
     }
 
-    // Method to handle the "Start Game" button press
-    private void OnStartGameButtonPressed()
+	/// =====================================
+	/// Button Functions
+	/// =====================================
+    private void OnStartButtonPressed()
     {
-        // Change the scene to the game scene
-        GetTree().ChangeSceneToFile("res://game_scene.tscn");
+        // Load the game scene
+        PackedScene gameScene = (PackedScene)GD.Load(GameScenePath);
+        
+        if (gameScene != null)
+        {
+            SceneTree tree = GetTree();
+            
+            Node gameSceneInstance = gameScene.Instantiate();
+            
+            tree.Root.AddChild(gameSceneInstance);
+            tree.CurrentScene.QueueFree();
+            tree.CurrentScene = gameSceneInstance;
+        }
+        else
+        {
+            GD.PrintErr("Failed to load the game scene at path: " + GameScenePath);
+        }
     }
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		Button startGameButton = GetNode<Button>("Start Game");
-		startGameButton.Connect("pressed", new Callable(this, nameof(OnStartGameButtonPressed)));
-	}
 }
