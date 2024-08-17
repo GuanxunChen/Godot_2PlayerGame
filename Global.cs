3d4b5495294d8b9b6ed1af245bb0c7fc88bf9d48
GuanxunChen <jadkkddhdh@gmail.com>
2024-08-16 21:22:54 -0400

    Update Global.cs

--- Global.cs
+++ Global.cs
@@ -2,7 +2,6 @@ using Godot;
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
-
 public partial class Global : Node
 {
 	public List<string> storylines = new List<string>();
@@ -15,24 +14,20 @@ public partial class Global : Node
         storylines.Add("2nd");
         storylines.Add("3rd");
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 }
+
 /*
-public class storyLines
+public class storyLine
 {
-    public string LeftCharacter { get; set; }
-    public string RightCharacters { get; set; }
-    public string currentLine { get; set; }
-    public string HighlightLorR { get; set; }
+    public string LCharacters { get; set; }
+    public string RCharacters { get; set; }
+    public string HighlightL { get; set; }
+    public string Line { get; set; }
 }
 
 public partial class Global : Node
 {
-	public List<storyLines> storylines = new List<storyLines>();
+	public List<storyLine> loadedStory = new List<storyLine>();
     public int currentLine = 0;
 
 	// Called when the node enters the scene tree for the first time.
