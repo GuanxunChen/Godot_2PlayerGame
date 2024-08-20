523656894bb3cb64cf3b36aeee8952dcb8919957
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-08-19 21:16:22 -0400

    made a camera node

--- Node2D2.cs
+++ Node2D2.cs
@@ -3,13 +3,25 @@ using System;
 
 public partial class Node2D2 : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    private Camera2D camera;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Ready()
+    {
+        camera = GetNode<Camera2D>("Camera2D");
+    }
+
+    public override void _Process(double delta)
+    {
+        // Zoom in using the '=' key (which is typically the same as the '+' key without Shift)
+        if (Input.IsKeyPressed(Key.Equal))
+        {
+            camera.Zoom *= new Vector2(0.9f, 0.9f); // Zoom out
+        }
+        
+        // Zoom out using the '-' key
+        if (Input.IsKeyPressed(Key.Minus))
+        {
+            camera.Zoom *= new Vector2(1.1f, 1.1f); // Zoom in
+        }
+    }
 }
