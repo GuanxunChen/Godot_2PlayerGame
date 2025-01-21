b50abf8c17941e08c8bb016692a3d8bdc4c450ff
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-01-20 21:22:11 -0500

    added choosing of hair color

--- CharacterCreate.cs
+++ CharacterCreate.cs
@@ -6,9 +6,9 @@ public partial class CharacterCreate : Node2D
     // Variables for customization
     private int currentHairstyle = 0;
     private Texture2D[] hairstyleImages = {
-        GD.Load<Texture2D>("res://art/temp,testing/characterCreation/hair1.jpg"),
-    	GD.Load<Texture2D>("res://art/temp,testing/characterCreation/hair2.jpg"),
-    	GD.Load<Texture2D>("res://art/temp,testing/characterCreation/hair3.jpg")
+        GD.Load<Texture2D>("res://art/temp,testing/characterCreation/hairstyles/hair1.jpg"),
+    	GD.Load<Texture2D>("res://art/temp,testing/characterCreation/hairstyles/hair2.jpg"),
+    	GD.Load<Texture2D>("res://art/temp,testing/characterCreation/hairstyles/hair3.jpg")
     };
     /*private string[] hairstyleTextures = {
         "res://hair1.jpg",
@@ -19,14 +19,17 @@ public partial class CharacterCreate : Node2D
     // Nodes for customization
     //private Sprite2D hairSprite;
     private TextureRect hairstylePreview;
+    private Sprite2D hairSprite;
 	private Button LeftArrow;
 	private Button RightArrow;
+    private HSlider redSlider, greenSlider, blueSlider;
 
     public override void _Ready()
     {
         // Get node references
         //hairSprite = GetNode<Sprite2D>("CharacterDisplay/Hair");
         hairstylePreview = GetNode<TextureRect>("Control/VBoxContainer/HBoxContainer/TextureRect");
+        hairSprite = GetNode<Sprite2D>("CharacterDisplay/Hair");
 
         
 
@@ -36,6 +39,14 @@ public partial class CharacterCreate : Node2D
 		RightArrow.Pressed += OnRightButtonPressed;
 
 		UpdateCharacter();
+
+        redSlider = GetNode<HSlider>("Control/VBoxContainer/redSlider");
+        greenSlider = GetNode<HSlider>("Control/VBoxContainer/greenSlider");
+        blueSlider = GetNode<HSlider>("Control/VBoxContainer/blueSlider");
+
+        redSlider.ValueChanged += OnSliderValueChanged;
+        greenSlider.ValueChanged += OnSliderValueChanged;
+        blueSlider.ValueChanged += OnSliderValueChanged;
     }
 
     // Change hairstyle based on direction
@@ -56,6 +67,7 @@ public partial class CharacterCreate : Node2D
         //hairSprite.Texture = GD.Load<Texture2D>(hairstyleTextures[currentHairstyle]);
         // Update preview image
         hairstylePreview.Texture = hairstyleImages[currentHairstyle];
+        hairSprite.Texture = hairstyleImages[currentHairstyle];
     }
 
     // Button signals
@@ -69,7 +81,22 @@ public partial class CharacterCreate : Node2D
         ChangeHairstyle(1);
     }
 
-    
+    private void OnSliderValueChanged(double value)
+    {
+        UpdateHairColor();
+    }
+
+    private void UpdateHairColor()
+    {
+        // Get RGB values from sliders
+        float red = (float)redSlider.Value / 255f;
+        float green = (float)greenSlider.Value / 255f;
+        float blue = (float)blueSlider.Value / 255f;
+
+        // Update the modulate color of the Sprite2D node
+        hairSprite.Modulate = new Color(red, green, blue);
+
+    }
 }
 
 
