5270b86b14834ea0bd3c1549db374b069dccb0e4
GuanxunChen <jadkkddhdh@gmail.com>
2025-02-12 21:48:40 -0500

    brought back the toggle

--- game_view.cs
+++ game_view.cs
@@ -4,19 +4,19 @@ using System.Runtime.CompilerServices;
 
 public partial class game_view : Node2D
 {
-    private Node2D prologueNode;
-    private Node2D mainStoryNode1;
+    private Node2D imageProcess;
+    private Node2D inputProcess;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         // Assume "Prologue" and "MainGameplay" are the names of the child nodes
-        prologueNode = GetNode<Node2D>("Prologue Node");
-        mainStoryNode1 = GetNode<Node2D>("MainStory Node 1");
+        imageProcess = GetNode<Node2D>("Image Process");
+        inputProcess = GetNode<Node2D>("Input Process");
 
         // Initially, the prologue is visible, and the main gameplay is hidden
-        prologueNode.Visible = true;
-        mainStoryNode1.Visible = false;
+        imageProcess.Visible = true;
+        inputProcess.Visible = false;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,23 +32,23 @@ public partial class game_view : Node2D
     private void SwitchToMainGameplay()
     {
         // Hide the prologue and show the main gameplay
-        if(prologueNode.Visible == false)
+        if(imageProcess.Visible == false)
         {
-            prologueNode.Visible = true;
-            mainStoryNode1.Visible = false;
+            imageProcess.Visible = true;
+            inputProcess.Visible = false;
 
             // Optionally, you can also stop the prologue processing if needed
-            prologueNode.SetProcess(true);
-            mainStoryNode1.SetProcess(false);
+            imageProcess.SetProcess(true);
+            inputProcess.SetProcess(false);
         }
         else
         {
-            prologueNode.Visible = false;
-            mainStoryNode1.Visible = true;
+            imageProcess.Visible = false;
+            inputProcess.Visible = true;
             
             // Optionally, you can also stop the prologue processing if needed
-            prologueNode.SetProcess(false);
-            mainStoryNode1.SetProcess(true);
+            imageProcess.SetProcess(false);
+            inputProcess.SetProcess(true);
         }
 
 
