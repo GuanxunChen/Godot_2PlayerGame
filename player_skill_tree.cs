ea56046299977f5a77851cee151a956911c8039b
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-02-28 16:25:21 -0500

    added many things to the skill tree

--- player_skill_tree.cs
+++ player_skill_tree.cs
@@ -13,6 +13,17 @@ public partial class player_skill_tree : Node2D
 	private Button backPassiveButton;
 	private Button backPetButton;
 	private Button backExplorationButton;
+	private Control attackControl;
+	private Control passiveControl;
+	private Control petControl;
+	private Control explorationControl;
+	private Label attackLabel;
+	private Line2D swordLine;
+	private Button crossSlash;
+	private Button starburstStream;
+	public int skillPoints = 10;
+	private Label skillPointsLabel;
+	private Label points;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -32,11 +43,37 @@ public partial class player_skill_tree : Node2D
 		backExplorationButton = GetNode<Button>("Panel/Exploration/Button");
 
 		playerButton = GetNode<TextureButton>("Panel/TextureButton");
+		attackLabel = GetNode<Label>("Panel/Attack/Label");
+
+		attackControl = GetNode<Control>("Panel/Attack/AttackControl");
+		passiveControl = GetNode<Control>("Panel/Passive/PassiveControl");
+		petControl = GetNode<Control>("Panel/Pet/PetControl");
+		explorationControl = GetNode<Control>("Panel/Exploration/ExplorationControl");
+
+		swordLine = GetNode<Line2D>("Panel/Attack/SwordLine");
+
+		crossSlash = GetNode<Button>("Panel/Attack/AttackControl/Sword/Cross Slash");
+		starburstStream = GetNode<Button>("Panel/Attack/AttackControl/Sword/Starburst Stream");
+
+		skillPointsLabel = GetNode<Label>("Panel/SkillPoints");
+		points = GetNode<Label>("Panel/SkillPoints/points");
 
 		backAttackButton.Visible = false;
 		backPassiveButton.Visible = false;
 		backPetButton.Visible = false;
 		backExplorationButton.Visible = false;
+		attackControl.Visible = false;
+		passiveControl.Visible = false;
+		petControl.Visible = false;
+		explorationControl.Visible = false;
+
+		starburstStream.Disabled = true;
+
+		swordLine.Visible = false;
+		points.Text = skillPoints.ToString();
+
+		skillPointsLabel.Visible = false;
+		skillPointsLabel.Position = new Vector2(0, 0);
 
 
 		// Connect the 'pressed' signal of the 'attackButton' node to the 'attacklabelclicked' method.
@@ -51,18 +88,69 @@ public partial class player_skill_tree : Node2D
 
 		explorationButton.Pressed += explorationButtonClicked;
 		backExplorationButton.Pressed += backButtonClicked;
+		crossSlash.Pressed += crossSlashClicked;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+	// Attack
 	public void attackButtonClicked()
 	{
 		camera.Position = new Vector2(960/2, 540/2);
 		camera.Zoom = new Vector2(2.0f, 2.0f);
 		playerButton.Visible = false;
 		backAttackButton.Visible = true;
+		attackControl.Visible = true;
+		attackLabel.Visible = false;
+		attackButton.Disabled = true;
+		swordLine.Visible = true;
+		skillPointsLabel.Visible = true;
+		skillPointsLabel.Position = new Vector2(832, 0);
+	}
+	public void crossSlashClicked()
+	{
+		starburstStream.Disabled = false;
+		skillPoints -= 1;
+		points.Text = skillPoints.ToString();
+	}
+	
+	// Pet
+	private void petButtonClicked()
+	{
+		camera.Position = new Vector2(1440, 540/2);
+		camera.Zoom = new Vector2(2.0f, 2.0f);
+		playerButton.Visible = false;
+		backPetButton.Visible = true;
+		petButton.Disabled = true;
+		petControl.Visible = true;
+		skillPointsLabel.Visible = true;
+		skillPointsLabel.Position = new Vector2(1792, 0);
+	}
+	// Passive
+	public void passiveButtonClicked()
+	{
+		camera.Position = new Vector2(960/2, 540 + (540/2));
+		camera.Zoom = new Vector2(2.0f, 2.0f);
+		playerButton.Visible = false;
+		backPassiveButton.Visible = true;
+		passiveButton.Disabled = true;
+		passiveControl.Visible = true;
+		skillPointsLabel.Visible = true;
+		skillPointsLabel.Position = new Vector2(832, 540);
+	}
+	// Exploration
+	public void explorationButtonClicked()
+	{
+		camera.Position = new Vector2(960 + (960/2), 540 + (540/2));
+		camera.Zoom = new Vector2(2.0f, 2.0f);
+		playerButton.Visible = false;
+		backExplorationButton.Visible = true;
+		explorationButton.Disabled = true;
+		explorationControl.Visible = true;
+		skillPointsLabel.Visible = true;
+		skillPointsLabel.Position = new Vector2(1792, 540);
 	}
 
 	public void backButtonClicked()
@@ -74,28 +162,18 @@ public partial class player_skill_tree : Node2D
 		backPassiveButton.Visible = false;
 		backPetButton.Visible = false;
 		backExplorationButton.Visible = false;
-	}
-
-	private void petButtonClicked()
-	{
-		camera.Position = new Vector2(1440, 540/2);
-		camera.Zoom = new Vector2(2.0f, 2.0f);
-		playerButton.Visible = false;
-		backPetButton.Visible = true;
-	}
-
-	public void passiveButtonClicked()
-	{
-		camera.Position = new Vector2(960/2, 540 + (540/2));
-		camera.Zoom = new Vector2(2.0f, 2.0f);
-		playerButton.Visible = false;
-		backPassiveButton.Visible = true;
-	}
-	public void explorationButtonClicked()
-	{
-		camera.Position = new Vector2(960 + (960/2), 540 + (540/2));
-		camera.Zoom = new Vector2(2.0f, 2.0f);
-		playerButton.Visible = false;
-		backExplorationButton.Visible = true;
+		attackButton.Visible = true;
+		attackControl.Visible = false;
+		passiveControl.Visible = false;
+		petControl.Visible = false;
+		explorationControl.Visible = false;
+		attackLabel.Visible = true;
+		attackButton.Disabled = false;
+		swordLine.Visible = false;
+		passiveButton.Disabled = false;
+		petButton.Disabled = false;
+		explorationButton.Disabled = false;
+		skillPointsLabel.Visible = false;
+		skillPointsLabel.Position = new Vector2(0, 0);
 	}
 }
